d2c7794c7c5680fc467e52fa44730174f70083ba
Chanhuyng Shim <cshim31@gatech.edu>
2021-11-14 23:28:49 -0500

    Update task.py

--- src/task.py
+++ src/task.py
@@ -10,7 +10,7 @@ def task(courseTerm):
     for courseSubjectKey,courseSubjectValue in courseSubjectPair.items():
         courseNums = crawl.fetchCourseNum(courseTerm, courseSubjectKey)
         for courseNum in courseNums:
-            print('fetcing %s:%s' % (courseSubjectKey, courseNum))
+            #print('fetcing %s:%s' % (courseSubjectKey, courseNum))
             courseList = crawl.fetchSchedule(courseTerm, courseSubjectValue, courseSubjectKey, courseNum)
             courseLists.append(courseList)
 
