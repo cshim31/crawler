bfbe9905949b8b3eade221d6706649524854a9ab
Chanhuyng Shim <cshim31@gatech.edu>
2021-11-14 21:27:42 -0500

    chore: update readme
    Update task.py
    
    Update task.py

--- src/task.py
+++ src/task.py
@@ -10,12 +10,13 @@ def task(courseTerm):
     for courseSubjectKey,courseSubjectValue in courseSubjectPair.items():
         courseNums = crawl.fetchCourseNum(courseTerm, courseSubjectKey)
         for courseNum in courseNums:
-            print('fetcing %s:%s' % (courseSubjectKey, courseNum))
+            #print('fetcing %s:%s' % (courseSubjectKey, courseNum))
             courseList = crawl.fetchSchedule(courseTerm, courseSubjectValue, courseSubjectKey, courseNum)
             courseLists.append(courseList)
 
     
     # write parsed data to file output in excel
-    print("Writing data...")
+    print("Writing csv...")
     parse.writeCSV(courseLists, courseTerm)
-    crawl.writeJson(courseLists, courseTerm)
+    print("Writing json...")
+    parse.writeJson(courseLists, courseTerm)
